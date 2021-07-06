3302afb3cb554e357f6591638e0e83f4bd17c9ba
solarjoni <26577692+solarjoni@users.noreply.github.com>
2021-07-06 10:30:57 +0000

    final

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 function basic_hasher(input) {
     
   // Up to you to write!
+  var hashval = 0;
 
+  for (var i = 0; i < input.length; i++) {
+    var unicode = input.charCodeAt(i);
+    hashval += unicode;
+    if (hashval > Number.MAX_SAFE_INTEGER) {
+      hashval = Number.MAX_SAFE_INTEGER;
+    }
+  }
+  return hashval;
 }
 
 // These are just some examples that you can use to test that your hashing function works! Feel free to replace the input string with whatever you like! The output gets printed when you run the file.
