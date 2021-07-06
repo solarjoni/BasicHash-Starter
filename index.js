d785eb19463105efe6ef0df2b0a3481265ca4a10
solarjoni <26577692+solarjoni@users.noreply.github.com>
2021-07-06 10:14:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// This function, basic_hasher(), is the function we're writing together. It's your job to fill it in!
+
+function basic_hasher(input) {
+    
+  // Up to you to write!
+
+}
+
+// These are just some examples that you can use to test that your hashing function works! Feel free to replace the input string with whatever you like! The output gets printed when you run the file.
+console.log(basic_hasher("boblover1234"));
+console.log(basic_hasher("TechRoulette!"));
